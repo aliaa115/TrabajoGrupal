45c86b79a663d4440a84977eb5b87bdec4a14b32
alia115 <labrild@miumg.edu.gt>
2018-05-28 15:35:08 -0600

    rombo de *

--- Ligia y Conny/Ligia y Conny/Ligia y conny.cpp
+++ Ligia y Conny/Ligia y Conny/Ligia y conny.cpp
@@ -3,6 +3,50 @@
 
 using namespace std;
 
+///=======PROTOTIPOS=======
+void programa1();
+
+
+///=======FUNCION MAIN=======
+
 void main() {
+	cout << "\t\tPROYECTO CON GITHUB Y HITKRAKEN DE PROGRAMACION 3\n\n";
 	cout << "hola mundo";
+
+	programa1();
+}
+
+
+///Programa 1
+	//Rombo de *
+void programa1() {
+	system("cls");
+	
+	int n;
+	cout << "\n\n\t\tROMBO DE ASTERISCOS:\n";
+	cout << "\tINGRESE LA ALTURA DEL ROMBO:  "; cin >> n;
+
+	cout << "\n\n";
+	for (int i = 0; i < n; i++) {
+		cout << "\t";
+		for (int j = 0; j < n - i - 1; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < 2 * i + 1; j++) {
+			cout << "*";
+		} cout << "\n";
+	}
+	for (int i = n - 2; i >= 0; i--) {
+		cout << "\t";
+		for (int j = 0; j < n - i - 1; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < 2 * i + 1; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	cout << "\n\n";
+	_getch();
+
 }
