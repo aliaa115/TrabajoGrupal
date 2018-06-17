21d9f90a6cd0a9a883979605cab8cafd7e28ebaa
alia115 <labrild@miumg.edu.gt>
2018-06-17 11:35:16 -0600

    Re-Inicio

---
+++ Ligia y Conny/Examen Final/Inicio.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <conio.h>
+#include "Menu.h"
+#include <string>
+
+void main() {
+	std::string eleccionmenu;
+	printf("\n\t\tPROYECTO CON GITHUB Y HITKRAKEN PARA EXAMEN FINAL DE PROGRAMACION 3:\n\tElija uno de los temas a continuacion para ejecutar el programa:\n"
+		"%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\tEleccion:  ", 
+		"\t\t 01 - pilas", 
+		"\t\t 02 - colas",
+		"\t\t 03 - listas simples", 
+		"\t\t 04 - listas dobles", 
+		"\t\t 05 - listas circulares",
+		"\t\t 06 - tablas hash ( numericas )", 
+		"\t\t 07 - tablas hash ( descriptivas )", 
+		"\t\t 08 - ", 
+		"\t\t 09 - ", 
+		"\t\t 10 - ", 
+		"\t\t 00 - SALIR");
+	std::cin >> eleccionmenu;
+	menu(eleccionmenu);
+	while (eleccionmenu != "0") {
+		system("cls");
+		printf("\n\t\tPROYECTO CON GITHUB Y HITKRAKEN PARA EXAMEN FINAL DE PROGRAMACION 3:\n\tElija uno de los temas a continuacion para ejecutar el programa:\n"
+			"%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\tEleccion:  ",
+			"\t\t 01 - pilas",
+			"\t\t 02 - colas",
+			"\t\t 03 - listas simples",
+			"\t\t 04 - listas dobles",
+			"\t\t 05 - listas circulares",
+			"\t\t 06 - tablas hash ( numericas )",
+			"\t\t 07 - tablas hash ( descriptivas )",
+			"\t\t 08 - ",
+			"\t\t 09 - ",
+			"\t\t 10 - ",
+			"\t\t 00 - SALIR"); 
+		std::cin >> eleccionmenu;
+		menu(eleccionmenu);
+	}
+	_getch();
+}
---
+++ Ligia y Conny/Examen Final/Menu.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+void menu(std::string opcion) {
+	int st = atoi(opcion.c_str());
+	switch (st){
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		break;
+	case 8:
+		break;
+	case 9:
+		break;
+	case 10:
+		break;
+	default:
+		break;
+	}
+
+}
