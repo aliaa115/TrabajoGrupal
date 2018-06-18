ba688902c2d99418d0a2df059fb0f1e9f53b2f90
alia115 <labrild@miumg.edu.gt>
2018-06-18 08:29:40 -0600

    _11

---
+++ Ligia y Conny/Examen Final/Menu.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "Hash_N.h"
+#include "Hash_L.h"
+#include "Lista_Doble.h"
+#include "floyd.h"
+//#include "Lista_Simple.h"
+
+void menu(std::string opcion) {
+	int st = atoi(opcion.c_str());
+	switch (st){
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		//main3();
+		break;
+	case 4:
+		main4();
+		break;
+	case 5:
+		break;
+	case 6:
+		main6();
+		break;
+	case 7:
+		main7();
+		break;
+	case 8:
+		main8();
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+		break;
+	case 9:
+		break;
+	case 10:
+		break;
+	default:
+		break;
+	}
+}
